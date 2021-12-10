54ea8c3c91703ac40382e9f550019244e794fb20
Shakir <shakirfarooq.baloch@gmail.com>
2021-12-10 08:28:44 +0500

    lock screen added

---
+++ app/src/main/java/com/muaz/mydiary/ui/lockscreen/LockActivity.java
@@ -0,0 +1,16 @@
+package com.muaz.mydiary.ui.lockscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muaz.mydiary.R;
+
+public class LockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+    }
+}
